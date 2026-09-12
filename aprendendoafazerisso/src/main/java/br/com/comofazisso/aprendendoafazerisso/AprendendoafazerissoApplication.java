bf6b2108c30f0e092ed7c4991debf94c2399cd14
nico-madruga <nizinhoms@gmail.com>
2026-09-12 16:11:21 -0300

    feat: Feito esqueleto do projeto

---
+++ aprendendoafazerisso/src/main/java/br/com/comofazisso/aprendendoafazerisso/AprendendoafazerissoApplication.java
@@ -0,0 +1,12 @@
+package br.com.comofazisso.aprendendoafazerisso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoafazerissoApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoafazerissoApplication.class, args);
+	}
+}
