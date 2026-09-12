4ffbe775c32bee87978cda62ebd5ec01dc702782
nico-madruga <nizinhoms@gmail.com>
2026-09-12 19:57:32 -0300

    feat: Feita entity usuario

--- aprendendoafazerisso/src/main/java/br/com/comofazisso/aprendendoafazerisso/entity/Usuario.java
+++ aprendendoafazerisso/src/main/java/br/com/comofazisso/aprendendoafazerisso/entity/Usuario.java
@@ -1,6 +1,27 @@
 package br.com.comofazisso.aprendendoafazerisso.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Usuario
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
 }
