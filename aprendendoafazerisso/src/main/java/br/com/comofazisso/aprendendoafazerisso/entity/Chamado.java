95bc19b94eecdfea28e748e082a8b42b61af5aea
nico-madruga <nizinhoms@gmail.com>
2026-09-12 19:55:46 -0300

    feat: Feita entity chamado

--- aprendendoafazerisso/src/main/java/br/com/comofazisso/aprendendoafazerisso/entity/Chamado.java
+++ aprendendoafazerisso/src/main/java/br/com/comofazisso/aprendendoafazerisso/entity/Chamado.java
@@ -1,6 +1,24 @@
 package br.com.comofazisso.aprendendoafazerisso.entity;
 
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Chamado
 {
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String status;
+    private String prioridade;
+    private LocalDateTime data_abertura;
 
 }
