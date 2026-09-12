b97b0c65c58224ef57ca438d4dfe17016045c9f6
nico-madruga <nizinhoms@gmail.com>
2026-09-12 19:56:18 -0300

    feat: Feita entity interacao

--- aprendendoafazerisso/src/main/java/br/com/comofazisso/aprendendoafazerisso/entity/Interacao.java
+++ aprendendoafazerisso/src/main/java/br/com/comofazisso/aprendendoafazerisso/entity/Interacao.java
@@ -1,6 +1,34 @@
 package br.com.comofazisso.aprendendoafazerisso.entity;
 
+import jakarta.persistence.*;
+import jdk.jfr.Timespan;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Interacao
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "mensagem")
+    private String mensagem;
+
+    @Column(name = "data")
+    private LocalDateTime data;
+
+    @OneToMany
+    @JoinColumn(name = "chamado")
+    private Chamado chamado;
+
+    @OneToMany
+    @JoinColumn(name = "tecnico")
+    private Tecnico tecnico;
 }
